26954814bd9a7a045d922c19b4c85ee6b3cde893
odootest <odootest2023@163.com>
2023-07-22 20:53:26 +0800

    support multi news in one row

--- src/components/NewsTitleList.jsx
+++ src/components/NewsTitleList.jsx
@@ -1,11 +1,27 @@
 
+import { useState } from 'react'
+import { Row, Col, Radio } from 'antd'
 import NewsTitle from './NewsTitle'
 
 export default ({ configs }) => {
 
+  const [span, setSpan] = useState(12)
+
+  const handleSpan = (e) => {
+    setSpan(+e.target.value)
+  }
+
   return (
     <>
-    {configs.map((d, i) => <NewsTitle config={d} key={i} />)}
+    <Radio.Group defaultValue={span.toString()} size="small" onChange={handleSpan}>
+      <Radio.Button value="24">1列</Radio.Button>
+      <Radio.Button value="12">2列</Radio.Button>
+      <Radio.Button value="8">3列</Radio.Button>
+      <Radio.Button value="6">4列</Radio.Button>
+    </Radio.Group>
+    <Row gutter={[16, 16]}>
+      {configs.map((d, i) => <Col span={span} key={i}><NewsTitle config={d} /></Col>)}
+    </Row>
     </>
   )
 }
