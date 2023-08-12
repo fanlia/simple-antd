c21ae1e267dc8f4419371bce512999eeb4c41139
odootest <odootest2023@163.com>
2023-08-12 21:03:54 +0800

    init proxy

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,64 +1,12 @@
 
-import { useState, useEffect } from 'react'
-import { Button, Form, Input, Space, message } from 'antd'
-
-import * as storage from '../services/storage'
-
-import { sendMail } from '../services/email'
+import SMTP from '../components/SMTP'
+import Proxy from '../components/Proxy'
 
 export default () => {
-  const [ form ] = Form.useForm()
-  const options = storage.get('mail-options') || {}
-
-  const onFinish = (values) => {
-    storage.set('mail-options', values)
-    message.success('提交成功')
-  }
-
-  const handleTest = async (e) => {
-    const values = form.getFieldsValue()
-
-    const mail = {
-      from: values.auth.user,
-      to: values.auth.user,
-      subject: "test",
-      html: "this is a test email",
-    }
-
-    const variables = {
-      options: {
-        port: 465,
-        secure: true,
-        ...values,
-      },
-      mail,
-    }
-
-    const res = await sendMail(variables)
-    if (res.data.send_mail) {
-      message.success('测试邮件发送成功')
-    } else {
-      message.error('测试邮件发送失败')
-    }
-  }
-
   return (
-    <Form
-      form={form}
-      initialValues={options}
-      onFinish={onFinish}
-      style={{ maxWidth: 600 }}
-      labelCol={{ span: 8 }}
-    >
-      <Form.Item label='SMTP' name='host' rules={[{ required: true }]}><Input /></Form.Item>
-      <Form.Item label='用户名' name={['auth', 'user']} rules={[{ required: true }]}><Input /></Form.Item>
-      <Form.Item label='密码' name={['auth', 'pass']} rules={[{ required: true }]}><Input.Password /></Form.Item>
-      <Form.Item wrapperCol={{ offset: 8 }}>
-        <Space>
-          <Button type='primary' htmlType='submit'>提交</Button>
-          <Button onClick={handleTest}>测试发送邮件</Button>
-        </Space>
-      </Form.Item>
-    </Form>
+    <>
+      <SMTP />
+      <Proxy />
+    </>
   )
 }
